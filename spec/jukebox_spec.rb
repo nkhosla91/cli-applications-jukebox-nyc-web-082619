0e8e493d6e7fb4ce1466d497cbaa57322aca80dc
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-27 13:42:26 +0000

    Automatically backed up by Learn

--- spec/jukebox_spec.rb
+++ spec/jukebox_spec.rb
@@ -28,8 +28,9 @@ describe "CLI Jukebox" do
 
     describe '#play' do
       it "can find a song when given a number from the user" do
-        allow(self).to receive(:gets).and_return("1")
+        
         expect( $stdout ).to receive(:puts).with(/Please enter a song name or number:/)
+        allow(self).to receive(:gets).and_return("1")
         expect( $stdout ).to receive(:puts).with(/Phoenix - 1901/)
         expect( $stdout ).to_not receive(:puts).with(/Tokyo Police Club - Wait Up/)
         play(songs)
