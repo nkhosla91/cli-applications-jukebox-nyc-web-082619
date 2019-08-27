4c7dbbaf9e09e49a3f128e56f165ab29f462e8fb
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-27 13:37:14 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,48 @@
 # Add your code here
+require 'pry'
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  i = 0
+  songs.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+    i += 1
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp
+  songs.each_with_index do |song, index|
+#binding.pry
+      if  input == (index + 1).to_s
+        puts "Playing #{song}"
+        #binding.pry
+      elsif  song == input
+        puts "Playing #{song}"
+      else
+        puts "Invalid input, please try again"
+      end
+  end
+end
+  
+def exit_jukebox
+  puts "Goodbye"
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
